1cf44051ad3ed5f2e034afd148798c7a3f2f945a
Caio Braga <caiotabraga@hotmail.com>
2023-08-04 16:57:18 -0300

    Implementing create new seller button

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -20,5 +20,10 @@ namespace SalesWebMvc.Controllers
             var list = SellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
